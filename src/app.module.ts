3b7180c0d3d78b244038a5a8adce8c547b7b7c31
Tan <ngoctana1k2014@gmail.com>
2023-12-02 21:05:03 +0700

    setup upload module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { PrismaModule } from './prisma/prisma.module';
       isGlobal: true,
     }),
     PrismaModule,
+    UploadModule,
   ],
   controllers: [],
   providers: [],
