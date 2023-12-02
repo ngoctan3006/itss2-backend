9bf38b55756cf2f8c376e68916225602e7c9e388
Tan <ngoctana1k2014@gmail.com>
2023-12-02 20:35:56 +0700

    setup app config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ENV_KEY } from './common/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  const prefix = configService.get<string>(ENV_KEY.PREFIX);
+  const port = configService.get<number>(ENV_KEY.PORT);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  app.setGlobalPrefix(prefix);
+  app.enableCors({ origin: '*' });
+
+  await app.listen(port || 3000);
 }
 bootstrap();
