33c15b1a01c5a357948bff64d7f80f45498b829b
Tan <ngoctana1k2014@gmail.com>
2023-12-02 20:44:00 +0700

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ENV_KEY } from './common/constants';
+import { setupSwagger } from './utils';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,8 @@ async function bootstrap() {
 
   app.setGlobalPrefix(prefix);
   app.enableCors({ origin: '*' });
+
+  setupSwagger(app, configService);
 
   await app.listen(port || 3000);
 }
---
+++ src/utils/swagger.util.ts
@@ -0,0 +1,23 @@
+import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ENV_KEY } from 'src/common/constants';
+
+export const setupSwagger = (
+  app: INestApplication,
+  configService: ConfigService,
+) => {
+  const config = new DocumentBuilder()
+    .setTitle(configService.get<string>(ENV_KEY.APP_NAME))
+    .setDescription('BKHome api documentation')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      basePath: '/v1',
+    },
+  });
+};
