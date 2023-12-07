dced494b21ed914bbe3a906b3fb0a274ef38dd34
Tan <ngoctana1k2014@gmail.com>
2023-12-08 01:33:06 +0700

    create room api

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { Room } from '@prisma/client';
+import { IResponse } from 'src/common/dtos';
+import { CreateRoomDto } from './dto';
+import { RoomService } from './room.service';
 
+@ApiTags('room')
 @Controller('room')
-export class RoomController {}
+export class RoomController {
+  constructor(private readonly roomService: RoomService) {}
+
+  @ApiConsumes(
+    'application/x-www-form-urlencoded',
+    'multipart/form-data',
+    'application/json',
+  )
+  @Post()
+  async create(@Body() data: CreateRoomDto): Promise<IResponse<Room>> {
+    return {
+      success: true,
+      message: 'Create room successfully',
+      data: await this.roomService.create(data),
+    };
+  }
+}
--- src/room/room.service.ts
+++ src/room/room.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Room } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateRoomDto } from './dto';
 
 @Injectable()
-export class RoomService {}
+export class RoomService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: CreateRoomDto): Promise<Room> {
+    return this.prisma.room.create({ data });
+  }
+}
