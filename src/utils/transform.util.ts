b7ff1934e403dcfd609e01d9492f2b5e45c195bb
Tan <ngoctana1k2014@gmail.com>
2023-12-08 03:25:37 +0700

    transform to number

--- src/utils/transform.util.ts
+++ src/utils/transform.util.ts
@@ -1,2 +1,8 @@
 export const transfromToBoolean = ({ value }: { value: string | boolean }) =>
   typeof value === 'boolean' ? value : value === 'true';
+
+export const transformToNumber = ({ value }: { value: string | number }) =>
+  Number(value);
+
+export const transformToInteger = ({ value }: { value: string | number }) =>
+  typeof value === 'number' ? value : parseInt(value);
