9b8c3ea06ed19ef8dbe5aacc491e525103f8fd16
Tan <ngoctana1k2014@gmail.com>
2023-12-09 23:42:11 +0700

    get user by id

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Query } from '@nestjs/common';
-import { ApiQuery, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
+import { ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { User } from '@prisma/client';
 import { IQuery, IResponse } from 'src/common/dtos';
 import { UserService } from './user.service';
@@ -21,5 +21,24 @@ export class UserController {
     @Query('search') search: string,
   ): Promise<IResponse<Array<Omit<User, 'password'>>>> {
     return await this.userService.findAll(params, search);
+  }
+
+  @ApiParam({
+    name: 'id',
+    required: true,
+    type: Number,
+    description: 'User ID',
+  })
+  @Get(':id')
+  async findOneById(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<IResponse<User>> {
+    const user = await this.userService.findOneById(id);
+    delete user.password;
+    return {
+      success: true,
+      message: 'Get user successfully',
+      data: user,
+    };
   }
 }
