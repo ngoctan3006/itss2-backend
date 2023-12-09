867cc1b883a2cffb62e41bbe228970fbe8a81697
Tan <ngoctana1k2014@gmail.com>
2023-12-09 23:23:20 +0700

    find user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UploadService } from 'src/upload/upload.service';
+import { CreateUserDto } from './dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly uploadService: UploadService,
+  ) {}
+
+  async findOneById(id: number): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+    if (!user) {
+      throw new NotFoundException({
+        success: false,
+        message: 'User not found',
+        data: null,
+      });
+    }
+    return user;
+  }
+
+  async findOneByUsername(username: string): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: { username },
+    });
+    if (!user) {
+      throw new NotFoundException({
+        success: false,
+        message: 'User not found',
+        data: null,
+      });
+    }
+    return user;
+  }
+
+  async create(data: CreateUserDto) {}
+}
