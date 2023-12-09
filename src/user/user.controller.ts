fae979dd17a5b1e5c8faa2d339c570f834312e12
Tan <ngoctana1k2014@gmail.com>
2023-12-09 23:33:26 +0700

    get all user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { User } from '@prisma/client';
+import { IQuery, IResponse } from 'src/common/dtos';
+import { UserService } from './user.service';
 
 @ApiTags('user')
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async findAll(
+    @Query() params: IQuery,
+  ): Promise<IResponse<Array<Omit<User, 'password'>>>> {
+    return await this.userService.findAll(params);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { User } from '@prisma/client';
+import { IQuery, IResponse } from 'src/common/dtos';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UploadService } from 'src/upload/upload.service';
 import { CreateUserDto } from './dto';
@@ -37,6 +38,36 @@ export class UserService {
       });
     }
     return user;
+  }
+
+  async findAll(
+    params: IQuery,
+  ): Promise<IResponse<Array<Omit<User, 'password'>>>> {
+    const { page, page_size, order_direction } = params;
+    return {
+      success: true,
+      message: 'Get all users successfully',
+      data: await this.prisma.user.findMany({
+        skip: page_size * (page - 1),
+        take: page_size,
+        orderBy: {
+          updated_at: order_direction,
+        },
+        select: {
+          id: true,
+          username: true,
+          avatar: true,
+          role: true,
+          created_at: true,
+          updated_at: true,
+        },
+      }),
+      pagination: {
+        page,
+        page_size,
+        total: await this.prisma.user.count(),
+      },
+    };
   }
 
   async create(data: CreateUserDto) {}
