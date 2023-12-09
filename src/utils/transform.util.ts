b02c0de262544c85ef7954171219d37cc8507a90
Tan <ngoctana1k2014@gmail.com>
2023-12-09 15:33:35 +0700

    update type

--- src/utils/transform.util.ts
+++ src/utils/transform.util.ts
@@ -1,8 +1,10 @@
-export const transfromToBoolean = ({ value }: { value: string | boolean }) =>
+import { TransformFnParams } from 'class-transformer';
+
+export const transfromToBoolean = ({ value }: TransformFnParams) =>
   typeof value === 'boolean' ? value : value === 'true';
 
-export const transformToNumber = ({ value }: { value: string | number }) =>
+export const transformToNumber = ({ value }: TransformFnParams) =>
   Number(value);
 
-export const transformToInteger = ({ value }: { value: string | number }) =>
+export const transformToInteger = ({ value }: TransformFnParams) =>
   typeof value === 'number' ? value : parseInt(value);
